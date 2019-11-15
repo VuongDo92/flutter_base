285ffc67321ede167037b130155721ce8d95c028
Vuong Do <vuongdt92@gmail.com>
2019-11-15 16:32:47 +0700

    create base_store in mobx

--- core/lib/stores/pagination_store.g.dart
+++ core/lib/stores/pagination_store.g.dart
@@ -13,95 +13,102 @@ mixin _$PaginationStore<T> on _PaginationStore<T>, Store {
 
   @override
   ObservableList<T> get items {
+    _$itemsAtom.context.enforceReadPolicy(_$itemsAtom);
     _$itemsAtom.reportObserved();
     return super.items;
   }
 
   @override
   set items(ObservableList<T> value) {
-    _$itemsAtom.context.checkIfStateModificationsAreAllowed(_$itemsAtom);
-    super.items = value;
-    _$itemsAtom.reportChanged();
+    _$itemsAtom.context.conditionallyRunInAction(() {
+      super.items = value;
+      _$itemsAtom.reportChanged();
+    }, _$itemsAtom, name: '${_$itemsAtom.name}_set');
   }
 
   final _$canLoadMoreAtom = Atom(name: '_PaginationStore.canLoadMore');
 
   @override
   bool get canLoadMore {
+    _$canLoadMoreAtom.context.enforceReadPolicy(_$canLoadMoreAtom);
     _$canLoadMoreAtom.reportObserved();
     return super.canLoadMore;
   }
 
   @override
   set canLoadMore(bool value) {
-    _$canLoadMoreAtom.context
-        .checkIfStateModificationsAreAllowed(_$canLoadMoreAtom);
-    super.canLoadMore = value;
-    _$canLoadMoreAtom.reportChanged();
+    _$canLoadMoreAtom.context.conditionallyRunInAction(() {
+      super.canLoadMore = value;
+      _$canLoadMoreAtom.reportChanged();
+    }, _$canLoadMoreAtom, name: '${_$canLoadMoreAtom.name}_set');
   }
 
   final _$isRefreshingAtom = Atom(name: '_PaginationStore.isRefreshing');
 
   @override
   bool get isRefreshing {
+    _$isRefreshingAtom.context.enforceReadPolicy(_$isRefreshingAtom);
     _$isRefreshingAtom.reportObserved();
     return super.isRefreshing;
   }
 
   @override
   set isRefreshing(bool value) {
-    _$isRefreshingAtom.context
-        .checkIfStateModificationsAreAllowed(_$isRefreshingAtom);
-    super.isRefreshing = value;
-    _$isRefreshingAtom.reportChanged();
+    _$isRefreshingAtom.context.conditionallyRunInAction(() {
+      super.isRefreshing = value;
+      _$isRefreshingAtom.reportChanged();
+    }, _$isRefreshingAtom, name: '${_$isRefreshingAtom.name}_set');
   }
 
   final _$isLoadingMoreAtom = Atom(name: '_PaginationStore.isLoadingMore');
 
   @override
   bool get isLoadingMore {
+    _$isLoadingMoreAtom.context.enforceReadPolicy(_$isLoadingMoreAtom);
     _$isLoadingMoreAtom.reportObserved();
     return super.isLoadingMore;
   }
 
   @override
   set isLoadingMore(bool value) {
-    _$isLoadingMoreAtom.context
-        .checkIfStateModificationsAreAllowed(_$isLoadingMoreAtom);
-    super.isLoadingMore = value;
-    _$isLoadingMoreAtom.reportChanged();
+    _$isLoadingMoreAtom.context.conditionallyRunInAction(() {
+      super.isLoadingMore = value;
+      _$isLoadingMoreAtom.reportChanged();
+    }, _$isLoadingMoreAtom, name: '${_$isLoadingMoreAtom.name}_set');
   }
 
   final _$paginationErrorAtom = Atom(name: '_PaginationStore.paginationError');
 
   @override
   Error get paginationError {
+    _$paginationErrorAtom.context.enforceReadPolicy(_$paginationErrorAtom);
     _$paginationErrorAtom.reportObserved();
     return super.paginationError;
   }
 
   @override
   set paginationError(Error value) {
-    _$paginationErrorAtom.context
-        .checkIfStateModificationsAreAllowed(_$paginationErrorAtom);
-    super.paginationError = value;
-    _$paginationErrorAtom.reportChanged();
+    _$paginationErrorAtom.context.conditionallyRunInAction(() {
+      super.paginationError = value;
+      _$paginationErrorAtom.reportChanged();
+    }, _$paginationErrorAtom, name: '${_$paginationErrorAtom.name}_set');
   }
 
   final _$refreshedAtAtom = Atom(name: '_PaginationStore.refreshedAt');
 
   @override
   DateTime get refreshedAt {
+    _$refreshedAtAtom.context.enforceReadPolicy(_$refreshedAtAtom);
     _$refreshedAtAtom.reportObserved();
     return super.refreshedAt;
   }
 
   @override
   set refreshedAt(DateTime value) {
-    _$refreshedAtAtom.context
-        .checkIfStateModificationsAreAllowed(_$refreshedAtAtom);
-    super.refreshedAt = value;
-    _$refreshedAtAtom.reportChanged();
+    _$refreshedAtAtom.context.conditionallyRunInAction(() {
+      super.refreshedAt = value;
+      _$refreshedAtAtom.reportChanged();
+    }, _$refreshedAtAtom, name: '${_$refreshedAtAtom.name}_set');
   }
 
   final _$refreshAsyncAction = AsyncAction('refresh');
