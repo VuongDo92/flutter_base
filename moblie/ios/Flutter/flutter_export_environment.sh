38f5b5c946db5efeba101cd3784979722077d044
Vuong Do <vuongdt92@gmail.com>
2019-12-28 15:01:02 +0700

    update config for ios build

--- moblie/ios/Flutter/flutter_export_environment.sh
+++ moblie/ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,10 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/kaneharry/Flutter_SDK/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/kaneharry/Projs/flutter_base/moblie"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/kaneharry/Projs/flutter_base/moblie/lib/development_main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/kaneharry/Flutter_SDK/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
