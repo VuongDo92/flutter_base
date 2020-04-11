3ef14703f1d128fa3aaf9ae11a92e060f4d75457
Vuong Do <vuongdt92@gmail.com>
2020-04-11 14:16:44 +0700

    upgrade

--- mobile/ios/Flutter/flutter_export_environment.sh
+++ mobile/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/kaneharry/Flutter_SDK/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/kaneharry/Projs/flutter_base/moblie"
+export "FLUTTER_APPLICATION_PATH=/Users/kaneharry/Projs/flutter_base/mobile"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
